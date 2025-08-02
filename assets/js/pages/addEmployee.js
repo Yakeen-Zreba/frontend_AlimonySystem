30cd049a7efbf09cdbbb27a66cb695695da67a5d
Yakeen-Zreba <yakeenzreba@gmail.com>
2025-08-02 12:20:03 +0200

    new

--- assets/js/pages/addEmployee.js
+++ assets/js/pages/addEmployee.js
@@ -1,38 +1,37 @@
 import { postData } from "../api/httpClient.js";
 import { validateRegisterData } from "../utils/validation.js";
 import { showError, hideError } from "../utils/helpers.js";
-
-
+console.log("addEmployee.js loaded");
 document.getElementById("addEmployeeForm").addEventListener("submit", async function (e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    const data = {
-        FirstName: document.getElementById("firstName").value.trim(),
-        MiddleName: document.getElementById("middleName").value.trim(),
-        LastName: document.getElementById("lastName").value.trim(),
-        PhoneNumber: document.getElementById("phone").value.trim(),
-        Email: document.getElementById("email").value.trim(),
-        PassportNumber: document.getElementById("passport").value.trim(),
-        Address: document.getElementById("address").value.trim(),
-        NID: document.getElementById("NID").value.trim(),
-        DateOfBirth: document.getElementById("birthdate").value,
-        Gender: document.querySelector("input[name='gender']:checked")?.value,
-        Nationality: document.querySelector("input[name='nationality']:checked")?.value,
-        Role: document.querySelector("input[name='role']:checked")?.value,
-        Username: document.getElementById("username").value.trim(),
-        Password: document.getElementById("password").value.trim(),
-        WorkDepartment: document.getElementById("workDepartment").value.trim(),
-    };
+  const data = {
+    FirstName: document.getElementById("firstName").value.trim(),
+    MiddleName: document.getElementById("middleName").value.trim(),
+    LastName: document.getElementById("lastName").value.trim(),
+    PhoneNumber: document.getElementById("phone").value.trim(),
+    Email: document.getElementById("email").value.trim(),
+    PassportNumber: document.getElementById("passport").value.trim(),
+    Address: document.getElementById("address").value.trim(),
+    NID: document.getElementById("NID").value.trim(),
+    DateOfBirth: document.getElementById("birthdate").value,
+    Gender: document.querySelector("input[name='gender']:checked")?.value,
+    Nationality: document.querySelector("input[name='nationality']:checked")?.value,
+    Role: document.querySelector("input[name='role_type']:checked")?.value,
+    Username: document.getElementById("username").value.trim(),
+    Password: document.getElementById("password").value.trim(),
+    WorkDepartment: document.getElementById("workDepartment").value.trim(),
+  };
 
-      hideError();
+  hideError();
 
-      const error = validateRegisterData(data);
-      if (error) {
-        showError(error);
-        return;
-      }
+  const error = validateRegisterData(data);
+  if (error) {
+    showError(error);
+    return;
+  }
 
- try {
+  try {
     const response = await fetch('https://localhost:44377/api/Person/add-employee', {
       method: 'POST',
       headers: {
@@ -42,14 +41,72 @@ document.getElementById("addEmployeeForm").addEventListener("submit", async func
     });
 
     if (response.ok) {
-        window.location.href = '../../html/admin/dashboard.html';
+        window.location.href = '../../html/admin/User-Management/government/view.html';
     } else {
       const result = await response.json();
-      showError(result.message || 'حدث خطأ أثناء التسجيل.');
+      showError(result.message || 'حدث خطأ أثناء الحفظ.');
     }
-    } catch (error) {
-      showError('خطأ في الاتصال بالخادم');
-    }
+  } catch (error) {
+    showError('خطأ في الاتصال بالخادم');
+  }
+});
 
-  });
 
+
+//  document.getElementById('formRegister').addEventListener('submit', async function (e) {
+//         e.preventDefault();
+
+//         const errorBox = document.getElementById('error-message');
+
+//         function showError(message) {
+//           errorBox.textContent = message;
+//           errorBox.classList.remove("d-none");
+//         }
+
+//         function hideError() {
+//           errorBox.classList.add("d-none");
+//           errorBox.textContent = "";
+//         }
+
+//         const data = {
+//           FirstName: document.getElementById("firstName").value.trim(),
+//           MiddleName: document.getElementById("middleName").value.trim(),
+//           LastName: document.getElementById("lastName").value.trim(),
+//           PhoneNumber: document.getElementById("phone").value.trim(),
+//           Email: document.getElementById("email").value.trim(),
+//           PassportNumber: document.getElementById("passport").value.trim(),
+//           Address: document.getElementById("address").value.trim(),
+//           NID: document.getElementById("NID").value.trim(),
+//           DateOfBirth: document.getElementById("birthdate").value,
+//           Gender: document.querySelector("input[name='gender']:checked")?.value,
+//           Nationality: document.querySelector("input[name='nationality']:checked")?.value,
+//           Role: document.querySelector("input[name='role_type']:checked")?.value,
+//           Username: document.getElementById("username").value.trim(),
+//           Password: document.getElementById("password").value.trim(),
+//         };
+
+//         hideError();
+//         try {
+//           const response = await fetch('https://localhost:44377/api/Person/Registration', {
+//             method: 'POST',
+//             headers: {
+//               'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify(data)
+//           });
+
+//           if (response.ok) {
+//             if(data.Role==='5')
+//               window.location.href = '../../html/divorced-woman/index.html';
+//             else if(data.Role==='2')
+//               window.location.href = '../../html/divorced-woman/index.html';
+//             else if(data.Role==='3')
+//               window.location.href = '../../html/divorced-man/index.html';
+//           } else {
+//             const result = await response.json();
+//             showError(result.message || 'حدث خطأ أثناء التسجيل.');
+//           }
+//         } catch (error) {
+//           showError('خطأ في الاتصال بالخادم');
+//         }
+//       });
--- assets/js/utils/validation.js
+++ assets/js/utils/validation.js
@@ -29,38 +29,6 @@ export function validateRegisterData(data) {
   return null;
 }
 
-
-export function validateRegisterData(data) {
-  if (!data.FirstName || !data.MiddleName || !data.LastName || !data.PhoneNumber || !data.Email ||
-      !data.PassportNumber || !data.Address || !data.NID || !data.DateOfBirth ||
-      !data.Gender || !data.Nationality || !data.Role || !data.Username || !data.Password) {
-    return "يرجى تعبئة جميع الحقول المطلوبة.";
-  }
-
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (!emailRegex.test(data.Email)) {
-    return "يرجى إدخال بريد إلكتروني صالح.";
-  }
-
-  if (data.Password.length < 8) {
-    return "يجب أن تحتوي كلمة المرور على 8 أحرف على الأقل.";
-  }
-
-    if (data.PhoneNumber.length < 10 || data.PhoneNumber.length > 12) {
-    return "يجب أن يحتوي رقم الهاتف  على 10 أرقام على الأقل.";
-  }
-
-  if (data.PassportNumber.length != 8) {
-    return "يجب أن يحتوي رقم جواز السفر  على 8 أرقام .";
-  }
-
-    if (data.NID.length != 12) {
-    return "يجب أن يحتوي الرقم الوطني  على 12 رقم .";
-  }
-
-  return null;
-}
-
 export function validateLoginData(data) {
   if (!data.Username || !data.Password) {
     return "يرجى تعبئة اسم المستخدم وكلمة المرور.";
